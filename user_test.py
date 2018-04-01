38f6e0dc224070c3ea3c44db47ef6cd0c52c99e9
mugala <mugalalevi21@gmail.com>
2018-04-01 20:33:58 +0300

    User_test This commit will add import the unittest module to the user_test module it will also import the Users class from the user_data module and finally create a subclass TestUsers and inherit unittest.TestCase. The docstrings tells us what the class will do.

---
+++ user_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module allows us to test
+from user_data import Users # Importing the users class from user_data module
+
+class TestUsers(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the users class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
