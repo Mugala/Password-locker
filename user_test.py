822d9fc18d43adc2692a63f5e408fb548e9c43a5
mugala <mugalalevi21@gmail.com>
2018-04-01 20:49:36 +0300

    Pycache file and capturing Error one This commit will add the Pycache file will and capture the first error: Ensuring that the objects are being instantiated correctly.

--- user_test.py
+++ user_test.py
@@ -9,3 +9,24 @@ class TestUsers(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user_data = Users ("Instagram","@levim","levi@email.com","axolotl") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user_data.account_name,"Instagram")
+        self.assertEqual(self.new_user_data.username,"@levim")
+        self.assertEqual(self.new_user_data.email,"levi@email.com")
+        self.assertEqual(self.new_user_data.password,"axolotl")
+
+
+if __name__ == '__main__':
+    unittest.main()
