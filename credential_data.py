a6334c296933409a804e752f9fb73cb57b5e2a19
mugala <mugalalevi21@gmail.com>
2018-04-02 15:29:52 +0300

    Credential data file This commit will add the file that will help generate the passwords

--- credential_data.py
+++ credential_data.py
@@ -5,3 +5,14 @@ class Credential:
     """
 
     pass
+
+    account_desc = [] #this is where the passwords will be held once generated
+
+    def __init__(self,gen_passcode,): #the intance variable
+
+         
+            self.gen_passcoode = gen_passcode
+
+
+    account_desc = [] # Empty container for storing passwords
+     # Init method up here
