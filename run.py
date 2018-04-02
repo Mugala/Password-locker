46054277dbe3ffbf75fba450124ff11eba6e3506
mugala <mugalalevi21@gmail.com>
2018-04-02 12:29:25 +0300

    This commit will  create functions that implement the behaviours that we have created

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3.6
 from user_data import Users
 
+
+def create_contact(account_name,username,email,password):
+    '''
+    Function to create account detail
+    '''
+    new_account = Users(account_name,username,email,password)
+    return new_account
+
+
