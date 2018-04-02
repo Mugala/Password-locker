d736eae324a3959b6fc572891e8cb3a5a835978e
mugala <mugalalevi21@gmail.com>
2018-04-02 12:33:38 +0300

    Save delete find check and dispay account functions This commit will  add the five functions to the run.py file and call the specific respective  methods

--- run.py
+++ run.py
@@ -10,3 +10,32 @@ def create_contact(account_name,username,email,password):
     return new_account
 
 
+def save_account(user_data):
+    '''
+    Function to the account details
+    '''
+    user_data.save_account()
+
+def del_account(user_data):
+    '''
+    Function to delete account details
+    '''
+    user_data.delete_account()
+
+def find_account(username):
+    '''
+    Function that finds account details using the username
+    '''
+    return Users.find_by_username(username)
+
+def check_existing_accounts(username):
+    '''
+    Function that check if a contact exists with that number and return a Boolean
+    '''
+    return Users.account_exist(username)
+
+def display_accounts():
+    '''
+    Function that returns all the saved contacts
+    '''
+    return Users.display_accounts()
